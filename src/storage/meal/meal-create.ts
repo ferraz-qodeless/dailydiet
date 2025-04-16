1b2491c91718e34eb4708f9f1de26a1bc0b1b4df
Renato Ferraz <renato.ferraz@qodeless.io>
2025-04-16 17:59:27 -0300

    Remover logs

--- src/storage/meal/meal-create.ts
+++ src/storage/meal/meal-create.ts
@@ -6,13 +6,8 @@ import { mealGetAll } from './meal-get-all';
 export async function mealCreate(newMeal: Meal) {
   try {
     const storedMeals = await mealGetAll();
-    console.log('Refeições salvas antes de adicionar a nova refeição:', storedMeals);
-
     const updatedMeals = [...storedMeals, newMeal];
-    console.log('Refeições após adicionar a nova refeição:', updatedMeals);
-
     await AsyncStorage.setItem(MEAL_COLLECTION, JSON.stringify(updatedMeals));
   } catch (error) {
-    console.error('Erro ao salvar a refeição:', error);
   }
 }
